bf6c7ec8ee737c7ad8659b499f5dd70fe4dbd1e7
ZeroTiem <maxhank22@gmail.com>
2022-04-11 02:16:56 +0800

    Add project files.

---
+++ Eight Queens/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Eight_Queens
+{
+    class Program
+    {
+        static int _solutionCount = 1;
+        static int _max = 8;
+        static int[] _row = new int[_max];
+        static string _resltRowFormat;
+
+        static void Main(string[] args)
+        {
+
+            _resltRowFormat = string.Create(7, '.', (span, c) =>
+            {
+                for (int i = 0; i < span.Length; i++) span[i] = c;
+            });
+
+            Run(0);
+
+            Console.Read();
+        }
+
+        private static void Run(int n)
+        {
+            if (n == _max) 
+            {
+                Print();
+                return;
+            }
+            for (int i = 0; i < _max; i++)
+            {
+                _row[n] = i;
+                if (IsAttack(n))
+                {
+                    Run(n + 1);
+                }
+            }
+        }
+
+        private static bool IsAttack(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (_row[i] == _row[n] || Math.Abs(n - i) == Math.Abs(_row[n] - _row[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine($"// Solution { _solutionCount++}");
+            for (int i = 0; i < _row.Length; i++)
+            {
+                System.Console.WriteLine(_resltRowFormat.Insert(_row[i], "q"));
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
